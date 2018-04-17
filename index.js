1c8adf8b5a485e5ad7f453d6d8eea489d85eff9c
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-17 18:47:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var recipes = { foo: "bar"}
 
 function updateObjectWithKeyAndValue(object, key, value)
 {
-  object[key] = value
-  return object
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 {
-
+  object[key] = value
+  return object
 }
 
 function deleteFromObjectByKey(object, key)

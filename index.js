4d894f77e30cdb9e3e26282be4d96bb63c3d7816
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-17 18:45:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var recipes = { foo: "bar"}
 
 function updateObjectWithKeyAndValue(object, key, value)
 {
-  var newobj = Object.assign({}, object)
-  newObj[key] = value
+  object[key] = value
   return object
 }
